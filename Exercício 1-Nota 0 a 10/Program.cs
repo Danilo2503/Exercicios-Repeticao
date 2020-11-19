26b153519b808b36adef00abb104f6ee400af634
unknown <dannilorch@gmail.com>
2020-11-19 11:16:58 -0300

    Exercicio 1

---
+++ Exercício 1-Nota 0 a 10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Exercício_1_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite uma nota:");
+            int nota = int.Parse(Console.ReadLine());
+            
+            while(nota <=0 || nota>10 ){
+                Console.WriteLine("Valor inválido");
+                Console.WriteLine("Digite um valor de 0 a 10");
+                nota = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Muito obrigado pelo seu tempo!");
+
+        }
+    }
+}
