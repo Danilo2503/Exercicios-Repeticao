265b8bbf8e95039056c61b71af968eb273d7d206
unknown <dannilorch@gmail.com>
2020-11-19 11:19:38 -0300

    Exercicio 2

---
+++ Exercício 2-Validação de Senha/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Exercício_2_Repetição
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------------");
+            Console.WriteLine("Validação de senha");
+            Console.WriteLine("------------------");
+
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite uma senha:");
+            string senha = Console.ReadLine();
+
+            while(nome == senha){
+                Console.WriteLine("Senha inválida - Digite uma diferente");
+                Console.WriteLine("Digite uma senha:");
+                senha = Console.ReadLine();
+            }
+            Console.WriteLine("Cadastro efetuado com sucesso");
+        }
+    }
+}
